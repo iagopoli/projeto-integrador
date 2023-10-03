d176dec99b601fe62efd5836819739f109945614
iagopoli <poliveiraiago19@gmail.com>
2023-10-03 19:57:16 -0300

    first commit

---
+++ resources/views/alimentaçao.blade.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="icon" type="image/x-icon" href="icone.ico">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Saúde</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+   <link rel="stylesheet" href="/css/alimentaçao.css">
+</head>
+
+<body>
+  <!-- inicio do nav bar-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+   <img class="imglogo" src="logo.png" alt="Terceiro Slide">
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Home <span class="sr-only">(página atual)</span></a>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Categorias
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="/alimentaçao">Alimentação</a>
+          <a class="dropdown-item" href="/habitos">Hábitos saudáveis</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Calculadoras
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="/IMC">IMC</a>
+          <a class="dropdown-item" href="/TMB">TMB</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="/sobre">Sobre</a>
+      </li>
+      </li>
+    </ul>
+
+  </div>
+</nav>
+ <!-- fim do navbar-->
+
+  <!-- inicio do carousel -->
+
+   <!-- fim do carousel -->
+
+
+   <div class="custom-div">
+   <h1 style="text-align: center;">Alimentação</h1><br>
+  <h6>
+A saúde não é apenas a ausência de doenças, mas sim um estado de equilíbrio físico, mental e emocional que nos permite desfrutar plenamente de cada aspecto da vida. Ela é a base sobre a qual construímos nossas conquistas, experiências e relacionamentos. Imagine seu corpo como uma máquina complexa e eficiente, e a saúde como o combustível que a mantém funcionando perfeitamente.
+</h6>
+
+       <div class="container">
+    <div class="image">
+      <img src="https://th.bing.com/th/id/OIG.6Y2B5krTO2KmbqmMzPPq?pid=ImgGn" alt="Imagem de Carboidratos">
+    </div>
+    <div class="text">
+      <h3>Carboidratos</h3>
+      <p>Os carboidratos são um dos três macronutrientes essenciais encontrados em nossa alimentação, ao lado de proteínas e gorduras. Eles desempenham um papel fundamental na obtenção de energia para o nosso corpo. Nesta seção, exploraremos os alimentos que são ricos em carboidratos e aqueles que contêm quantidades mais baixas desse nutriente, para ajudá-lo a fazer escolhas alimentares informadas.</p>
+    </div>
+  </div>
+
+<br>
+     <br>
+
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+
+      <th scope="col"><h4>Alimentos Ricos em Carboidratos</h4></th>
+      <th scope="col"><h4>Alimentos com Menos Carboidratos</h4></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td scope="row"><h5>Grãos Integrais </h5><p>Grãos como arroz integral, quinoa, aveia e trigo integral são excelentes fontes de carboidratos complexos, que fornecem energia sustentável.</p></td>
+      <td><h5>Vegetais de Folhas Verdes</h5><p>Alimentos como espinafre, couve e alface contêm quantidades muito baixas de carboidratos, sendo ideais para dietas com restrição desse nutriente.</p></td>
+
+    </tr>
+    <tr>
+      <td scope="row"><h5> Leguminosas</h5> <p>Feijões, lentilhas e grão-de-bico são ricos em carboidratos e também fornecem uma boa quantidade de fibras e proteínas.</p></td>
+      <td><h5> Brócolis e Couve-Flor </h5> <p>Esses vegetais crucíferos têm baixo teor de carboidratos e são ricos em fibras e nutrientes.</p></td>
+
+    </tr>
+    <tr>
+      <td scope="row"><h5>Tubérculos</h5> <p>Batatas, batata-doce e mandioca são tubérculos que contêm carboidratos complexos e são uma parte importante da dieta de muitas culturas.</p> </td>
+      <td colspan="2"><h5>Abacate </h5> <p>Embora seja uma fruta, o abacate possui uma quantidade relativamente baixa de carboidratos, mas é rico em gorduras saudáveis.</p> </td>
+
+    </tr>
+     <tr>
+      <td scope="row"><h5>Frutas </h5> <p>Bananas, maçãs, uvas e mangas são conhecidas por seu teor de carboidratos naturais, principalmente na forma de frutose.</p> </td>
+      <td colspan="2"><h5>Nozes e Sementes</h5> <p> Nozes, amêndoas e sementes de chia são boas opções para pessoas que desejam limitar a ingestão de carboidratos enquanto obtêm gorduras saudáveis e proteínas.</p> </td>
+
+    </tr>
+     <tr>
+      <td scope="row"><h5>Produtos de Panificação </h5><p> Pães, massas e cereais são fontes comuns de carboidratos refinados, que devem ser consumidos com moderação, preferindo as versões integrais sempre que possível.</p></td>
+      <td colspan="2"><h5>Carne e Peixe</h5><p>Carnes magras, como peito de frango e peixes, contêm quantidades insignificantes de carboidratos, tornando-os adequados para dietas com baixo teor desse nutriente.</p></td>
+
+    </tr>
+  </tbody>
+</table>
+     <br>
+     <h5>Lembre-se de que a quantidade de carboidratos que você deve consumir depende das suas necessidades individuais, estilo de vida e objetivos. É importante escolher fontes de carboidratos de qualidade, como grãos integrais e vegetais, e limitar a ingestão de carboidratos refinados e processados. Sempre consulte um profissional de saúde ou nutricionista antes de fazer grandes mudanças em sua dieta,Nossa equipe está aqui para fornecer informações e ajudá-lo a fazer escolhas alimentares informadas. Seja qual for o seu estilo de vida, entender os carboidratos é um passo importante em direção a uma vida saudável e equilibrada.</h5>
+
+       <div class="custom-container">
+    <div class="custom-text">
+    <h3>Carboidratos</h3>
+    <p>Os carboidratos são um dos três macronutrientes  essenciais encontrados em nossa alimentação, ao lado de proteínas e gorduras. Eles desempenham um papel fundamental na obtenção de energia para o nosso corpo. Nesta seção, exploraremos os alimentos que são ricos em carboidratos e aqueles que contêm quantidades mais baixas desse nutriente, para ajudá-lo a fazer escolhas alimentares informadas.</p>
+  </div>
+  <div class="custom-image">
+    <img src="https://clinicagenics.com/wp-content/uploads/2018/08/genics-reproducao-humana-fertilidade-feminina-3.jpeg" alt="Imagem de Carboidratos">
+  </div>
+  </div>
+     <br>
+     <h1 style="text-align: center;">Tabela De Alimentos</h1><br>
+     <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">100g</th>
+        <th scope="col">Carboidratos</th>
+        <th scope="col">Proteinas</th>
+        <th scope="col">Calorias</th>
+        <th scope="col">Valor energetico</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">ARROZ</th>
+        <td>200g</td>
+        <td>2qq2</td>
+        <td>arrascae</td>
+        <td>arrascae</td>
+      </tr>
+      <tr>
+        <th scope="row">FEIJÃO</th>
+        <td>arrasca</td>
+        <td>arrasc</td>
+        <td>arras</td>
+        <td>arrascae</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>arra</td>
+        <td>arr</td>
+        <td>ar</td>
+        <td>arrascae</td>
+      </tr>
+
+
+      <tr>
+        <th scope="col">4</th>
+        <td scope="col">a</td>
+        <td scope="col">ar</td>
+        <td scope="col">arr</td>
+        <td>arrascae</td>
+      </tr>
+
+      <tr>
+        <th scope="row">5</th>
+        <td>arra</td>
+        <td>arras</td>
+        <td>arrasc</td>
+        <td>arrascae</td>
+      </tr>
+      <tr>
+        <th scope="row">6</th>
+        <td>arrasca</td>
+        <td>arrascae</td>
+        <td>arrascaet</td>
+        <td>arrascae</td>
+      </tr>
+      <tr>
+        <th scope="row">7</th>
+        <td>arrascaeta</td>
+        <td>lindo</td>
+        <td>@perfeito</td>
+        <td>arrascae</td>
+      </tr>
+    </tbody>
+  </table>
+
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+   <script src="saude.js"></script>
+
+  <br>
+<br>
+
+
+    <!-- FOOTER -->
+
+  <div class="d-flex flex-column h-100">
+
+    <footer class="w-100 py-4 flex-shrink-0">
+        <div class="container py-4">
+            <div class="row gy-4 gx-5">
+                <div class="col-lg-4 col-md-6">
+                    <h5 class="h1 text-white">Viver bem</h5>
+                    <p class="small text-muted"> Site informativo sobre alimentação saudável e informações essenciais para o pleno funcionamento do corpo humano</p>
+                    <p class="small text-muted mb-0">&copy; Copyrights. All rights reserved. <a class="text-primary" href="https://classroom.google.com/u/0/c/NTg4OTkyOTAxMDE1?hl=pt-BR">projetointegrador</a></p>
+                </div>
+                <div class="col-lg-2 col-md-6">
+                    <h5 class="text-white mb-3">Inicio</h5>
+                    <ul class="list-unstyled text-muted">
+                        <li><a href="/">Home</a></li>
+
+                    </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-6">
+                    <h5 class="text-white mb-3">Suporte</h5>
+                        <ul class="list-unstyled text-muted">
+                        <li><a href="/faq">FAQ</a></li>
+
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+    <!-- FIM DO FOOTER -->
+
+</body>
+</html>
---
+++ resources/views/faq.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" type="image/x-icon" href="icone.ico">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>FAQ - Perguntas Frequentes</title>
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="/css/faq.css">
+</head>
+<body>
+<br>
+  <br>
+<div class="faq-container">
+  <h1>Perguntas Frequentes</h1>
+  <br>
+  <br>
+  <div class="faq-item">
+    <h2>O que é a Taxa Metabólica Basal (TMB)?</h2>
+    <p>A Taxa Metabólica Basal (TMB) é a quantidade de calorias que o seu corpo queima em repouso para manter as funções vitais.</p>
+  </div>
+  <div class="faq-item">
+    <h2>Como posso calcular minha TMB?</h2>
+    <p>Você pode calcular sua TMB usando a fórmula de Harris-Benedict, que leva em consideração seu peso, altura e idade.</p>
+  </div>
+  <div class="faq-item">
+    <h2>Qual é a interpretação da TMB?</h2>
+    <p>A interpretação da TMB varia de acordo com a faixa de valores. Valores mais baixos podem indicar um metabolismo lento, enquanto valores mais altos indicam um metabolismo mais rápido.</p>
+
+<div class="faq-container">
+
+
+  <div class="faq-item">
+    <h2>O que é o Índice de Massa Corporal (IMC)?</h2>
+    <p>O IMC é uma medida que relaciona o peso de uma pessoa com sua altura, usada para avaliar se o peso está dentro de faixas consideradas saudáveis.</p>
+  </div>
+
+  <div class="faq-item">
+    <h2>Como calcular o IMC?</h2>
+    <p>O IMC é calculado dividindo o peso da pessoa (em quilogramas) pela altura ao quadrado (em metros). A fórmula é IMC = peso / (altura * altura).</p>
+  </div>
+
+  <div class="faq-item">
+    <h2>Qual é a interpretação dos valores de IMC?</h2>
+    <p>O IMC é interpretado em faixas, sendo: abaixo de 18.5 (Abaixo do Peso), 18.5 - 24.9 (Peso Normal), 25 - 29.9 (Sobrepeso), 30 - 34.9 (Obesidade Grau 1), 35 - 39.9 (Obesidade Grau 2) e acima de 40 (Obesidade Grau 3).</p>
+  </div>
+  </div>
+</div>
+</div>
+  <br>
+  <br>
+    <a href="/" class="back-button">Voltar</a>
+  <br>
+  <br>
+  <br>
+
+  <div class="d-flex flex-column h-100">
+
+    <footer class="w-100 py-4 flex-shrink-0">
+        <div class="container py-4">
+            <div class="row gy-4 gx-5">
+                <div class="col-lg-4 col-md-6">
+                    <h5 class="h1 text-white">Viver bem</h5>
+                    <p class="small text-muted"> Site informativo sobre alimentação saudável e informações essenciais para o pleno funcionamento do corpo humano</p>
+                    <p class="small text-muted mb-0">&copy; Copyrights. All rights reserved. <a class="text-primary" href="https://classroom.google.com/u/0/c/NTg4OTkyOTAxMDE1?hl=pt-BR">projetointegrador</a></p>
+                </div>
+                <div class="col-lg-2 col-md-6">
+                    <h5 class="text-white mb-3">Inicio</h5>
+                    <ul class="list-unstyled text-muted">
+                        <li><a href="/">Home</a></li>
+
+                    </ul>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <h5 class="text-white mb-3">Suporte</h5>
+                        <ul class="list-unstyled text-muted">
+                        <li><a href="/faq">FAQ</a></li>
+
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- FIM DO FOOTER -->
+</body>
+</html>
